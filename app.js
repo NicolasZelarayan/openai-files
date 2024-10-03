fbe10b20f7ee8cf65d8a537ee864726ee55ea558
Nicolas <nicolaszelarayan.ez@gmail.com>
2024-10-03 15:29:25 -0300

    fix env

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.delete('/files/:id', async (req, res) => {
   }
 });
 
+// definimos puerto
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
